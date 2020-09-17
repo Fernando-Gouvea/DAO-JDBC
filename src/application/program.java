56f171a007d6a61de254cc19a5ef72c57abda607
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-09-17 16:56:39 -0300

    Department entity class

--- src/application/program.java
+++ src/application/program.java
@@ -1,43 +1,16 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class program {
 
 	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"Books");
+		System.out.println(obj);
 
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.createStatement();
-			
-			
-			rs = st.executeQuery("select * from biblioteca.livro");
-			
-			while (rs.next()) {
-				System.out.println(rs.getInt("cod_livro") + "," + rs.getString("titulo") + "," + rs.getString("nome_editora"));
-				
-			}
-			
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			
-		}
-		
 	}
 }
-
 	
 
 
