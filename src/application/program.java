a1f1e7d79035ce04e4c19500f24dde8a245f0842
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-09-24 19:33:03 -0300

    findByDepartment

--- src/application/program.java
+++ src/application/program.java
@@ -3,6 +3,8 @@ package application;
 
 import java.sql.Connection;
 import java.util.Date;
+import java.util.List;
+
 import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,16 +15,25 @@ public class program {
 
 	public static void main(String[] args) {
 		
-		//Connection conn = DB.getConnection();
-		//DB.closeConnection();
-		
-		//Department obj = new Department(1,"Books");
-		//Seller seller = new Seller(55,"Jose maria", "Ze@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		Seller seller = sellerDao.findById(3);
 		System.out.print("------Teste 1 ---findById------\n");
 		System.out.println(seller);
+		
+		System.out.println("\n-----Teste 2-----findById------\n");
+		Department department =  new Department(2 ,null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+		
+		
+		
 
 	}
 }
